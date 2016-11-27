6fc63409082eff60656e3101506fbdcc54160253
meg4r0m <psykoterro@gmail.com>
2016-11-27 23:06:24 +0100

    woops again

--- exercice5.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: meg4r0m
- * Date: 27/11/16
- * Time: 22:38
- */
-$HautsDeFrance = array(59=>"Nord",80=>"Somme",62=>"Pas-de-Calais",02=>"Aisne",60=>"Oise");
-echo var_dump($HautsDeFrance);
