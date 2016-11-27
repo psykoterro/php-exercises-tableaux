6ff7739fba091dbd9b1d24ca4cae9e16bbb63dc9
meg4r0m <psykoterro@gmail.com>
2016-11-27 23:09:13 +0100

    Exercices Faits

---
+++ exercice8.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: meg4r0m
+ * Date: 27/11/16
+ * Time: 22:47
+ */
+$mois = array("janvier","fevrier","mars","avril","mai","juin","juillet","aout","septembre","octobre","novembre","decembre");
+
+foreach ($mois as $moisaff){
+    echo $moisaff."<br />";
+}
